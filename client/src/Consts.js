390f0339f7292c899eaec5385c8b0d7a19414c7c
CAT-i0n <rudman.iv.o@gmail.com>
2026-06-23 12:17:56 +0300

    Update table consts

---
+++ client/src/Consts.js
@@ -0,0 +1,18 @@
+
+export const CARD_SIZE = 40
+export const VIEW_BOX_SIZE = {
+    x: 1000,
+    y: 650
+}
+export const CENTER_COORDS = {
+    x: VIEW_BOX_SIZE.x/2,
+    y: VIEW_BOX_SIZE.y/2
+}
+
+export const TABLE_PARAMS = {
+    x: VIEW_BOX_SIZE.x*0.35,
+    y: VIEW_BOX_SIZE.y*0.25
+}
+
+
+export const WINDOW_RATIO = VIEW_BOX_SIZE.x/VIEW_BOX_SIZE.y
